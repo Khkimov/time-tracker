08a567100e0941c9b2deaef573ca5cc7e6a82874
Khkimov <khakim198765@gmail.com>
2024-01-05 11:47:35 +0500

    refactor button const

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -30,3 +30,8 @@ export const PERIOD_SELECT_OPTIONS = [
     value: 45
   }
 ]
+
+export const BUTTON_TYPE_NEUTRAL = 'neutral'
+export const BUTTON_TYPE_DANGER = 'danger'
+
+export const BUTTON_TYPES = [BUTTON_TYPE_NEUTRAL, BUTTON_TYPE_DANGER]
--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,11 +1,26 @@
 <template>
   <div>
     <button
-      class="rounded bg-gray-100 p-3 enabled:hover:bg-gray-200 disabled:cursor-not-allowed disabled:opacity-50"
+      :class="`${typeClasses[type]}rounded p-3 disabled:cursor-not-allowed disabled:opacity-50`"
     >
       <slot />
     </button>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { BUTTON_TYPE_NEUTRAL, BUTTON_TYPE_DANGER } from '@/common/constants'
+
+interface Props {
+  type: string
+}
+
+withDefaults(defineProps<Props>(), {
+  type: BUTTON_TYPE_NEUTRAL
+})
+
+const typeClasses = {
+  [BUTTON_TYPE_NEUTRAL]: 'bg-gray-100 enabled:hover:bg-gray-200',
+  [BUTTON_TYPE_DANGER]: 'bg-red-500 enabled:hover:bg-red-600 text-white'
+}
+</script>
